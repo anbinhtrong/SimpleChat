7146b3714ec65bff0691fea2d1804e966bf37ff6
Án Bình Trọng <minhtam.seta@gmail.com>
2013-12-10 15:08:50 +0700

    Implement Simple Chat: broadcast message except own client's message

--- serverChat.js
+++ serverChat.js
@@ -6,9 +6,18 @@ chatServer.on('connection', function(client){
 	client.write('Server chat program\n');
 	client.on('data', function(data){
 		console.log(data.toString());
-		for(var i = 0; i < clientChats.length; i++){
-			clientChats[i].write(data);
-		}
+//		for(var i = 0; i < clientChats.length; i++){
+//			clientChats[i].write(data);
+//		}
+		broadcast(data, client);
 	});
 });
+
+function broadcast(message, client){
+	for(var i = 0; i < clientChats.length; i++){
+		if(client != clientChats[i]){
+			clientChats[i].write(message);
+		}
+	}
+}
 chatServer.listen(1001);
