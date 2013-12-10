06792ed5b38aa66c1377cb33927171b520add43e
Án Bình Trọng <minhtam.seta@gmail.com>
2013-12-10 13:38:25 +0700

    Add Eclipse Nodejs Project

--- serverChat.js
+++ serverChat.js
@@ -1,9 +1,14 @@
 var net = require('net');
 var chatServer = net.createServer();
+var clientChats = [];
 chatServer.on('connection', function(client){
+	clientChats.push(client);
 	client.write('Server chat program\n');
 	client.on('data', function(data){
 		console.log(data.toString());
+		for(var i = 0; i < clientChats.length; i++){
+			clientChats[i].write(data);
+		}
 	});
 });
 chatServer.listen(1001);
